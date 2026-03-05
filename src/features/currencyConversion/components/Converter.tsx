eb5e92652b4b1838f0c28718dfe0ba1308442572
rrowinski <rrowinskipl@gmail.com>
2026-03-05 13:16:29 +0100

    add shadcn, add basic components, add hook form, add api key config, add api proxy, add prettier config, add .env file with api key

---
+++ src/features/currencyConversion/components/Converter.tsx
@@ -0,0 +1,6 @@
+export const Converter = () => {
+
+    return (<div className="grid w-full max-w-sm gap-6">
+
+    </div>);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/api/currencybeacon': {
+        target: 'https://api.currencybeacon.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/currencybeacon/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq) => {
+            proxyReq.setHeader('Authorization', `Bearer ${env.CURRENCY_BEACON_API_KEY}`);
+          });
+        }
+      }
+    }
+  }
+}})
